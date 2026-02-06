a2082e57f0c5b31f55a3734306cbe84a5ba2dea2
Du <119209610+PhanBaDu@users.noreply.github.com>
2026-02-07 00:10:04 +0700

    feat: Implement a new dashboard layout and refresh UI for administrative pages including category, product, and employee listings.

--- SV22T1020073.Admin/obj/Debug/net10.0/SV22T1020073.Admin.AssemblyInfo.cs
+++ SV22T1020073.Admin/obj/Debug/net10.0/SV22T1020073.Admin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SV22T1020073.Admin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1d497cff88102e8e2996271c859568a3c15fb54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd904c797a19e4a8f7ff69e1d79a41e46454b28d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SV22T1020073.Admin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SV22T1020073.Admin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
